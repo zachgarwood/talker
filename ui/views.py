04a9d9e441375c1bfc36e429890e2064b9b61281
Dan Madere <dan@danmadere.com>
2018-10-20 21:44:02 -0400

    Refactor with goal of not repeating JS code or list of supported words
    - Reduce instances of hard-coded words to one
    - JS uses event listeners rather than hard coded onclick's
    - More console.log details for debugging

--- ui/views.py
+++ ui/views.py
@@ -3,48 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     #this is your new views
-    images = [
-    {'click': "my.play()", 'source':"/images/my.jpg"},
-    {'click': "please.play()", 'source':"/images/please.jpg"},
-    {'click': "that.play()", 'source':"/images/that.jpg"},
-    {'click': "and.play()", 'source':"/images/and.jpg"},
-    {'click': "what.play()", 'source':"/images/what.jpg"},
-    {'click': "a.play()", 'source':"/images/a.jpg"},
-    {'click': "there.play()", 'source':"/images/there.jpg"},
-    {'click': "i.play()", 'source':"/images/i.jpg"},
-    {'click': "we.play()", 'source':"/images/we.jpg"},
-    {'click': "are.play()", 'source':"/images/are.jpg"},
-    {'click': "is.play()", 'source':"/images/is.jpg"},
-    {'click': "were.play()", 'source':"/images/were.jpg"},
-    {'click': "was.play()", 'source':"/images/was.jpg"},
-    {'click': "you.play()", 'source':"/images/you.jpg"},
-    {'click': "they.play()", 'source':"/images/they.jpg"},
-    {'click': "play.play()", 'source':"/images/play.jpg"},
-    {'click': "like.play()", 'source':"/images/like.jpg"},
-    {'click': "work.play()", 'source':"/images/work.jpg"},
-    {'click': "have.play()", 'source':"/images/have.jpg"},
-    {'click': "feel.play()", 'source':"/images/feel.jpg"},
-    {'click': "read.play()", 'source':"/images/read.jpg"},
-    {'click': "more.play()", 'source':"/images/more.jpg"},
-    {'click': "stop.play()", 'source':"/images/stop.jpg"},
-    {'click': "it.play()", 'source':"/images/it.jpg"},
-    {'click': "he.play()", 'source':"/images/he.jpg"},
-    {'click': "want.play()", 'source':"/images/want.jpg"},
-    {'click': "come.play()", 'source':"/images/come.jpg"},
-    {'click': "go.play()", 'source':"/images/go.jpg"},
-    {'click': "say.play()", 'source':"/images/say.jpg"},
-    {'click': "color.play()", 'source':"/images/color.jpg"},
-    {'click': "help.play()", 'source':"/images/help.jpg"},
-    {'click': "she.play()", 'source':"/images/she.jpg"},
-    {'click': "dont.play()", 'source':"/images/dont.jpg"},
-    {'click': "eat.play()", 'source':"/images/eat.jpg"},
-    {'click': "make.play()", 'source':"/images/make.jpg"},
-    {'click': "need.play()", 'source':"/images/need.jpg"},
-    {'click': "drink.play()", 'source':"/images/drink.jpg"},
-    {'click': "turn.play()", 'source':"/images/turn.jpg"},
-    {'click': "put.play()", 'source':"/images/put.jpg"}
-    ]
-    return render(request, 'index.html', {'images': images})
+    words = ["my", "please", "that", "and", "what", "a", "there", "i", "we", "are", "is", "were", "was", "you", "they",
+             "play", "like", "work", "have", "feel", "read", "more", "stop", "it", "he", "want", "come", "go", "say",
+             "color", "help", "she", "dont", "eat", "make", "need", "drink", "turn", "put"]
+
+    # build a list of dictionaries with "word" and "image_url" keys
+    word_images = []
+    for word in words:
+        word_images.append({'word': word,
+                            'image_url': '/images/{}.jpg'.format(word)})
+
+    return render(request, 'index.html', {'word_images': word_images})
 
 #TODO populate images dictionary with records from database via Model
 
