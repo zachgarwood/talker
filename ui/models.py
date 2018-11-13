8afee718c735088aadae45e361c9cb141106f000
Zach Garwood <zachgarwood@gmail.com>
2018-11-12 22:36:44 -0600

    Removed unnecessary Audio and Image models

--- ui/models.py
+++ ui/models.py
@@ -1,11 +1,4 @@
 from django.db import models
 
-# Create your models here.
-class Audio(models.Model):
-    audio_file = models.CharField(max_length=30)
-
-class Images(models.Model):
-    image_file = models.CharField(max_length=30)
-
 #TODO create a class with both audio and image filefield type elements
 #that will handle user uploads
